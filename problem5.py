f1eb1f9b1701bda63758fb0754a6806ba1076634
Aanshikgupta <65184197+Aanshikgupta@users.noreply.github.com>
2020-05-14 14:03:37 +0530

    Delete problem5.py

--- problem5.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter no of elements of the list :-   "));st=[];nondup=[]
-for i in range(n):
-    ele=input()
-    st.append(ele)
-for x in st:
-    if (x not in nondup):
-        nondup.append(x)
-print(nondup)
