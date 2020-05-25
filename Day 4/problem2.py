1ced80878dc3df78f446691741e77e202e8d1b66
Aanshikgupta <65184197+Aanshikgupta@users.noreply.github.com>
2020-05-25 19:44:41 +0530

    Add files via upload

---
+++ Day 4/problem2.py
@@ -0,0 +1,9 @@
+lst = [(4, 5, 1), (6, 1, 5), (7, 4, 2), (6, 2, 4)] 
+   
+print("The original list is : " + str(lst)) 
+  
+N = 1
+ 
+lst.sort(key = lambda x: x[N]) 
+
+print("List after sorting tuple by Nth index sort : " + str(lst)) 
